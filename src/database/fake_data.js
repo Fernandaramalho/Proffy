a12c7a4633a23a113711786b699c25a484a325af
Fernandaramalho <fernandadosantos021999@gmail.com>
2020-08-09 19:21:41 -0300

    Plataforma de proffesor pronta

---
+++ src/database/fake_data.js
@@ -0,0 +1,35 @@
+const proffys = [ 
+  { 
+    name: "Fernanda Ramalho",
+    avatar:"https://avatars1.githubusercontent.com/u/40431760?s=460&v=4",
+    whatsapp: "21989313651",
+    bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+    subject:"Química", 
+    cost:"40", 
+    wekday:[0], 
+    time_from:[720],
+     time_to:[1220]
+    },
+    { 
+      name: "Maria Luciene",
+      avatar:"https://avatars1.githubusercontent.com/u/40431760?s=460&v=4",
+      whatsapp: "989313651",
+      bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+      subject:"Química", 
+      cost:"40", 
+      wekday:[1], 
+      time_from:[720],
+       time_to:[1220]
+      },
+      { 
+        name: "Sofia seila",
+        avatar:"https://avatars1.githubusercontent.com/u/40431760?s=460&v=4",
+        whatsapp: "989313651",
+        bio:"Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
+        subject:"Inglês", 
+        cost:"20", 
+        wekday:[1], 
+        time_from:[720],
+         time_to:[1220]
+        }
+]
